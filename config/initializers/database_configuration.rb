25f4191d40def03ba37766f151fc6bc49af75860
Raphael Campean <raphaelcampean76@gmail.com>
2023-09-08 11:26:52 -0300

    pp

--- config/initializers/database_configuration.rb
+++ config/initializers/database_configuration.rb
@@ -1,3 +1,3 @@
 Rails.application.configure do
     config.active_record.default_database_url = ENV['postgresql://postgres:wSP7t6x7nEJKJT5RlCGO@containers-us-west-34.railway.app:7323/railway']
-  end
+end
