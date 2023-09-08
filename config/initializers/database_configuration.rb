257d0e0cb899f3390c884a629d15ca113e3b3481
Raphael Campean <raphaelcampean76@gmail.com>
2023-09-08 11:37:39 -0300

    ss

--- config/initializers/database_configuration.rb
+++ config/initializers/database_configuration.rb
@@ -1,3 +1,3 @@
 Rails.application.configure do
-    config.active_record.default_shard = ENV['postgresql://postgres:wSP7t6x7nEJKJT5RlCGO@containers-us-west-34.railway.app:7323/railway']
+    config.active_record.default_shard = 'shard1'
 end
